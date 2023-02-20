ae6e26d1fabba7d64d9399a575c486f5e42b4922
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-20 12:07:39 +0100

    base integration tests and integration package

---
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package pl.lotto;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.lotto.domain.LottoSpringBootApplication;
+
+@SpringBootTest(classes = LottoSpringBootApplication.class)
+public class BaseIntegrationTest {
+
+}
