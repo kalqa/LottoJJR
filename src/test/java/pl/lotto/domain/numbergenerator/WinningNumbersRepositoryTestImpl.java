c59dee76c804a710fe4df9148e69912f1561c3d8
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-21 13:47:04 +0100

    added mongorepository, and spring data, unit tests implementations

--- src/test/java/pl/lotto/domain/numbergenerator/WinningNumbersRepositoryTestImpl.java
+++ src/test/java/pl/lotto/domain/numbergenerator/WinningNumbersRepositoryTestImpl.java
@@ -1,17 +1,25 @@
 package pl.lotto.domain.numbergenerator;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 
 public class WinningNumbersRepositoryTestImpl implements WinningNumbersRepository {
 
     private final Map<LocalDateTime, WinningNumbers> winningNumbersList = new ConcurrentHashMap<>();
 
     @Override
-    public WinningNumbers save(WinningNumbers winningNumbers) {
-        return winningNumbersList.put(winningNumbers.date(), winningNumbers);
+    public <S extends WinningNumbers> S save(S entity) {
+        winningNumbersList.put(entity.date(), entity);
+        return entity;
     }
 
     @Override
@@ -23,5 +31,115 @@ public class WinningNumbersRepositoryTestImpl implements WinningNumbersRepositor
     public boolean existsByDate(LocalDateTime nextDrawDate) {
         winningNumbersList.get(nextDrawDate);
         return true;
+    }
+
+    @Override
+    public <S extends WinningNumbers> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<WinningNumbers> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public List<WinningNumbers> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<WinningNumbers> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(WinningNumbers entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends WinningNumbers> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<WinningNumbers> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<WinningNumbers> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends WinningNumbers> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends WinningNumbers> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends WinningNumbers> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends WinningNumbers, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
     }
 }
