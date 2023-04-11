6cd64db26254f1465a87484d479fcd09fc29601b
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-04-11 14:40:49 +0200

    added frontend to docker-compose and generalized localhost cors policy

--- src/main/java/pl/lotto/infrastructure/cors/CorsConfig.java
+++ src/main/java/pl/lotto/infrastructure/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
